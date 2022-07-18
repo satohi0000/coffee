ddc03ce2f448f4683a3a61343a817f78030bbb51
satohi0000 <87967374+satohi0000@users.noreply.github.com>
2022-07-18 11:48:34 +0900

    reset

--- js/style.js
+++ js/style.js
@@ -1,14 +1,1 @@
 // タグ実装
-$(".c-tab-toggle").on("click", function () {
-  $(this)
-    .closest(".c-tab-container")
-    .find(".c-tab-content")
-    .removeClass("show");
-  $(this)
-    .closest(".c-tab-container")
-    .find(".c-tab-content")
-    .eq($(this).index())
-    .addClass("show");
-  $(this).closest(".c-tab-container").find(".c-tab-toggle").removeClass("on");
-  $(this).addClass("on");
-});
