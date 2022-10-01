71f0557aeb9060687158c95b470028df3bfcb4de
satohi0000 <87967374+satohi0000@users.noreply.github.com>
2022-10-02 03:07:13 +0900

    pulldown 対応

--- js/style.js
+++ js/style.js
@@ -1,1 +1,8 @@
-// タグ実装
+$('a[href^="#"]').click(function () {
+  const speed = 500;
+  let href = $(this).attr("href");
+  let target = $(href == "#" || href == "" ? "html" : href);
+  let position = target.offset().top;
+  $("body,html").animate({ scrollTop: position }, speed, "swing");
+  return false;
+});
